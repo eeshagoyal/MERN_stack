be6b24cc27c79de1177e485b5d5c7da0b03a2e84
Eesha Goyal <eeshagoyal96@gmail.com>
2018-05-10 22:07:07 +0530

    trying heroku again

--- index.js
+++ index.js
@@ -9,4 +9,9 @@ app.get('/',(req,res) => {
 	res.send({hi : 'there'});
 });
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+/*environment variable 
+dnamically assigned port by heroku after deployment 
+or port 5000 for local development environment
+*/
+app.listen(PORT);
